cdce4aab0972753f3d473c5e8da8fc04327c3db8
George Chkhvirkia <georgechkhvirkia@gmail.com>
2016-06-12 15:24:51 +0400

    changed serializer

--- app/serializers/suggestion_serializer.rb
+++ app/serializers/suggestion_serializer.rb
@@ -3,6 +3,6 @@ class SuggestionSerializer < ActiveModel::Serializer
 
   def suggestion
     query = @instance_options[:search_string]
-    suggestion = object.name.include?(query) ? object.name : object.address
+    object.name.include?(query) ? object.name : object.address
   end
 end
